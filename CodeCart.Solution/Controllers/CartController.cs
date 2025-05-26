a2b68f70eab1d79d2d2bfd2b836bc13d5906e4ba
Ahmed Usama <ausama@innotech.com.eg>
2025-05-26 21:07:24 +0300

    CartController

---
+++ CodeCart.Solution/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using CodeCart.Core.Entities;
+using CodeCart.Core.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeCart.API.Controllers;
+
+
+public class CartController(ICartService cartService) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<ShoppingCart>> GetCartById(string id)
+    {
+        var cart =await cartService.GetCartAsync(id);
+
+        return Ok(cart ?? new ShoppingCart { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ShoppingCart>> UpdateCart(ShoppingCart cart)
+    {
+        var updatedCart =await cartService.SetCartAsync(cart);
+
+        if (updatedCart == null) return BadRequest("Problem with Cart");
+
+        return Ok(updatedCart);
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult>DeleteCart(string id)
+    {
+        var result = await cartService.DeleteCartAsync(id);
+
+        if (!result)
+            return BadRequest("Problem deleting cart");
+
+        return NoContent();
+    }
+}
