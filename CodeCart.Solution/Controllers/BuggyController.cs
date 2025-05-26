be4aa8a4f86e5bcd1105fba43895f5dd67bb0d6b
Ahmed Usama <ausama@innotech.com.eg>
2025-05-26 14:53:14 +0300

    Making buggy controller to test apierrors handling

---
+++ CodeCart.Solution/Controllers/BuggyController.cs
@@ -0,0 +1,43 @@
+﻿using CodeCart.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeCart.API.Controllers
+{
+
+    public class BuggyController : BaseApiController
+    {
+
+        [HttpGet("not-found")]
+        public IActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+
+        [HttpGet("bad-request")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest("This is a bad request");
+        }
+
+
+        [HttpGet("unauthorized")]
+        public IActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("server-error")]
+        public IActionResult GetServerError()
+        {
+            throw new Exception("This is a server error");
+        }
+
+        [HttpPost("validation-error")]
+        public IActionResult GetValidationError(CreateProductDto product)
+        {
+            return BadRequest("Please provide required values");
+        }
+    }
+}
