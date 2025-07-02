b2718fb8f36e60187ee0889e6586a42e51bb09b8
Ahmed Usama <ausama@innotech.com.eg>
2025-07-02 14:39:11 +0300

    finishing payment succeed webhook

--- CodeCart.Solution/Controllers/PaymentsController.cs
+++ CodeCart.Solution/Controllers/PaymentsController.cs
@@ -11,9 +11,10 @@ namespace CodeCart.API.Controllers;
 
 public class PaymentsController(IPaymentService paymentService,
     IUnitOfWork unitOfWork,
-    ILogger<PaymentsController> logger) : BaseApiController
+    ILogger<PaymentsController> logger,
+    IConfiguration configuration) : BaseApiController
 {
-    private readonly string _whSecret = "";
+    private readonly string _whSecret = configuration["StripeSettings:WhSecret"]!;
 
     [Authorize]
     [HttpPost("{cartId}")]
@@ -41,8 +42,7 @@ public class PaymentsController(IPaymentService paymentService,
         try
         {
             var stripeEvent = ConstructStripeEvent(json);
-
-            if(stripeEvent.Data.Object is not PaymentIntent intent)
+            if (stripeEvent.Data.Object is not PaymentIntent intent)
             {
                 return BadRequest("Invalid event data");
             }
@@ -68,9 +68,14 @@ public class PaymentsController(IPaymentService paymentService,
         if(intent.Status == "succeeded")
         {
             var spec = new OrderSpecifications(intent.Id, true);
-            var order = await unitOfWork.Repository<Order>().GetEntityWithSpecAsync(spec)??throw new Exception("Order not found");
+            var order = await unitOfWork.Repository<Order>().GetEntityWithSpecAsync(spec);
+            if (order == null)
+            {
+                logger.LogWarning("Order not found for PaymentIntentId: {IntentId}", intent.Id);
+                return;
+            }
 
-            if((long)(order.Subtotal+order.DeliveryMethod.Price) * 100 != intent.Amount)
+            if ((long)(order.Subtotal+order.DeliveryMethod.Price) * 100 != intent.Amount)
             {
                 order.Status = OrderStatus.PaymentMismatch;
             }
